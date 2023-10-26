aa16452b1df666ff9c3060e57b01ae9c4735723c
oa180 <oa20180180@gmail.com>
2023-10-26 22:44:33 +0300

    trying to solve prisma error

--- Database/prisma/migrations/20231026192923_first_migration/migration.sql
+++ Database/prisma/migrations/20231026192923_first_migration/migration.sql
@@ -1,6 +1,12 @@
 -- CreateEnum
 CREATE TYPE "RoleEnum" AS ENUM ('ADMIN', 'CLIENT', 'INSTRUCTOR');
 
+-- CreateEnum
+CREATE TYPE "GenderEnum" AS ENUM ('male', 'female', 'notdefined');
+
+-- CreateEnum
+CREATE TYPE "SessionStatusEnum" AS ENUM ('pending', 'canceled', 'running', 'deliverd');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -9,6 +15,7 @@ CREATE TABLE "User" (
     "handler" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "gender" "GenderEnum" NOT NULL DEFAULT 'notdefined',
     "country" TEXT,
     "phone" TEXT,
     "photo" TEXT,
@@ -53,22 +60,21 @@ CREATE TABLE "UserTopics" (
 -- CreateTable
 CREATE TABLE "Session" (
     "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "delivered" BOOLEAN NOT NULL,
-    "capacity" INTEGER NOT NULL,
-    "startTime" TIMESTAMP(3) NOT NULL,
-    "endTime" TIMESTAMP(3) NOT NULL,
-    "instructorId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "notes" TEXT,
+    "status" "SessionStatusEnum" NOT NULL DEFAULT 'pending',
+    "instructorHandler" TEXT,
+    "clientHandler" TEXT,
 
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "clientSession" (
-    "clientId" TEXT NOT NULL,
-    "sessionId" TEXT NOT NULL,
+CREATE TABLE "SessionTopics" (
+    "topicId" TEXT NOT NULL,
+    "seesionId" TEXT NOT NULL,
 
-    CONSTRAINT "clientSession_pkey" PRIMARY KEY ("clientId","sessionId")
+    CONSTRAINT "SessionTopics_pkey" PRIMARY KEY ("topicId","seesionId")
 );
 
 -- CreateTable
@@ -100,13 +106,16 @@ ALTER TABLE "UserTopics" ADD CONSTRAINT "UserTopics_userId_fkey" FOREIGN KEY ("u
 ALTER TABLE "UserTopics" ADD CONSTRAINT "UserTopics_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Session" ADD CONSTRAINT "Session_instructorId_fkey" FOREIGN KEY ("instructorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Session" ADD CONSTRAINT "Session_instructorHandler_fkey" FOREIGN KEY ("instructorHandler") REFERENCES "User"("handler") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "clientSession" ADD CONSTRAINT "clientSession_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Session" ADD CONSTRAINT "Session_clientHandler_fkey" FOREIGN KEY ("clientHandler") REFERENCES "User"("handler") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "clientSession" ADD CONSTRAINT "clientSession_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "SessionTopics" ADD CONSTRAINT "SessionTopics_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SessionTopics" ADD CONSTRAINT "SessionTopics_seesionId_fkey" FOREIGN KEY ("seesionId") REFERENCES "Session"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "BookedSlot" ADD CONSTRAINT "BookedSlot_instructorId_fkey" FOREIGN KEY ("instructorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
