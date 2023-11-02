4da169754147645f0b506ffd58cc092a544cc855
oa180 <oa20180180@gmail.com>
2023-11-02 11:30:30 +0200

    remove gender from register form

--- src/modules/User/userController.js
+++ src/modules/User/userController.js
@@ -52,7 +52,7 @@ export const getAllClients = catchAsync(async (req, res, next) => {
 });
 
 export const registerNewUser = catchAsync(async (req, res, next) => {
-  const { firstName, lastName, email, handler, password } = req.body;
+  const { firstName, lastName, email, handler, password, gender } = req.body;
   const { error, value } = createUserValidation.validate({
     firstName,
     lastName,
@@ -63,11 +63,11 @@ export const registerNewUser = catchAsync(async (req, res, next) => {
   });
   if (error) return next(new AppError(error, 400));
   const hashedPassword = await argon.hash(password);
-  // const tempPhoto =
-  //   gender == 'male'
-  //     ? 'https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg'
-  //     : 'https://png.pngtree.com/png-clipart/20190904/original/pngtree-circular-pattern-user-cartoon-avatar-png-image_4492893.jpg';
-  // // create New User
+  const tempPhoto =
+    gender == 'male'
+      ? 'https://png.pngtree.com/png-clipart/20200224/original/pngtree-cartoon-color-simple-male-avatar-png-image_5230557.jpg'
+      : 'https://png.pngtree.com/png-clipart/20190904/original/pngtree-circular-pattern-user-cartoon-avatar-png-image_4492893.jpg';
+  // create New User
   const newUser = await prisma.user.create({
     data: {
       firstName,
