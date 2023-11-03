9677b9fa9386f902a91d9d65933634e094cb8136
oa180 <oa20180180@gmail.com>
2023-11-03 17:03:39 +0200

    Add Get All Countries

--- src/modules/Topics/topicsRouter.js
+++ src/modules/Topics/topicsRouter.js
@@ -2,13 +2,18 @@
 
 import express from 'express';
 
-import { getAllTopics, createNewTopic } from './topicController.js';
+import {
+  getAllTopics,
+  createNewTopic,
+  getAllCountries,
+} from './topicController.js';
 
 import { authenticate } from '../../../middlewares/auth/Authentication.js';
 import { isAuthorized } from '../../../middlewares/auth/Authorization.js';
 const router = express.Router();
 
 router.get('/', getAllTopics);
+router.get('/countries', getAllCountries);
 
 router.post('/new', authenticate, isAuthorized, createNewTopic);
 
